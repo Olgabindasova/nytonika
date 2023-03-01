677e9356af9fad6d17d9aac6c140d484871858d4
Olgabindasova <47353846+Olgabindasova@users.noreply.github.com>
2023-03-01 16:45:47 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,50 @@
+
+
 $(document).ready(function(){
  
-    $(window).scroll(function(){
-    if ($(this).scrollTop() > 100) {
-    $('.scrollup').fadeIn();
-    } else {
-    $('.scrollup').fadeOut();
-    }
-    });
-     
-    $('.scrollup').click(function(){
-    $("html, body").animate({ scrollTop: 0 }, 600);
-    return false;
-    });
-     
-    });
+  $(window).scroll(function(){
+  if ($(this).scrollTop() > 100) {
+  $('.scrollup').fadeIn();
+  } else {
+  $('.scrollup').fadeOut();
+  }
+  });
+   
+  $('.scrollup').click(function(){
+  $("html, body").animate({ scrollTop: 0 }, 600);
+  return false;
+  });
+   
+  });
+
+$(function() {
+
+  $("#phone").mask("+7 (999) 999-99-99");
+  $(".phone").mask("+7 (999) 999-99-99");
+});
 
 
-    $(function() {
+$(document).ready(function () {
 
-		$("#phone").mask("+7 (999) 999-99-99");
+  var show = true;
+  var countbox = ".benefits__inner";
+  $(window).on("scroll load resize", function () {
+      if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+      var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+      var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+      var w_height = $(window).height(); // Высота окна браузера
+      var d_height = $(document).height(); // Высота всего документа
+      var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+      if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+          $('.benefits__number').css('opacity', '1');
+          $('.benefits__number').spincrement({
+              thousandSeparator: "",
+              duration: 1000
+          });
 
-	});
+          show = false;
+      }
+  });
 
+}); 
 
